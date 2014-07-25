43e1fcd06168b6d6dcfb92d707c67628f869aad7
Edwin Coleman <dwinicoleman@gmail.com>
2014-07-25 15:14:01 +1000

    first change to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions [1] consruct an artificial matrix by analogy to makeVector
+## [2] given matrix as input, check cache for its inverse;
+## if found, return it, else calculate the invere and cahe it
 
-## Write a short comment describing this function
+## This functin creates the artifical matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function does the  inverting and caching, or fetching the inverse, as required
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
