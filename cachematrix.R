18986ef495950171630efe9e4b492338b0c27476
Edwin Coleman <dwinicoleman@gmail.com>
2014-07-27 08:31:33 +1000

    added explanations

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,17 @@
+## Rprograming, Assignment 2
+
 ## These functions [1] consruct an artificial matrix by analogy to makeVector
 ## [2] given matrix as input, check cache for its inverse;
 ## if found, return it, else calculate the invere and cahe it
 
-## This function creates the artificial matrix
+## This function creates the artificial matrix, which is a lst of four functions, set, get, setinverse and getinverse
+## what each does is explained below inline
+
+## To use these functions, first assign to a variable with makeCacheMatrix
+## as in
+##   x <- matrix(c(1,2,33,49),nrow=2,ncol=2); b <- makeCacheMatrix(x)
+## then you can get the inverse with cacheSolve(b)
+## and agai,n but this time from cache!! Woo hoo.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -25,15 +34,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function does the  inverting and caching, or fetching the inverse, as required
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of x
+  
   m <- x$getinverse()
   if(!is.null(m)) {
-    message("getting cached data")
+    
+    ## if the inverse is in cache, m is given its value which is returned
+    message("getting cached data") 
     return(m)
   }
-  data <- x$get()
+  ## otherwise, the inverse function (solve) is appled to x
+  data <- x$get()  
   m <- solve(data)
-  x$setinverse(m)
+  
+  ## the inverse just found is cached and returned
+  x$setinverse(m)  
   m
   
   
